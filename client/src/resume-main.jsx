7d5b1d069887e7aed0f219a59543e1201665c004
Deekshitha Puppala <deekshithapuppala5@gmail.com>
2026-03-03 16:52:12 +0530

    Fix resume import path and build issue

--- client/src/resume-main.jsx
+++ client/src/resume-main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Resume from "./pages/Resumeesume";
+import Resume from "./pages/resume";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
